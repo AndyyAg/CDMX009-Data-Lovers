a146251f2fbc96f65703a8b0ba684c583b8497a3
Mara Mulato <marianamendozamulato@MacBook-Mara.local>
2020-03-10 10:40:34 -0600

    modifiqué src

--- src/data.js
+++ src/data.js
@@ -2,15 +2,26 @@
 // import data from './data/lol/lol.js';
 // import data from './data/patient/patient.js';
 // import data from './data/pokemon/pokemon.js';
-import data from './data/rickandmorty/rickandmorty.js'; 
+//import data from './data/rickandmorty/rickandmorty.js';
 // import data from './data/steam/steam.js';
 // import data from './data/steam/worldbank.js';
 
 // esta es una función de ejemplo
 
-export const data = () => {
-  return 'data';
-};
+//export const data = () => {
+//  return 'data';
+//};
+
+var api = 'https://rickandmortyapi.com/api/';
+fetch (api)
+ .then((response) => {
+   return response.json();
+ })
+   .then((data) => {
+     console.log(data);
+   });
+
+document.querySelector('#cosadata').addEventListener('click', fetch);
 
 //Manipulación a través de arreglos y objetos
 
