8f1f388e6196d917c91b0677bb3837c952341b2f
Mara Mulato <marianamendozamulato@MacBook-Mara.local>
2020-03-13 14:28:23 -0600

    js comentado

--- src/data.js
+++ src/data.js
@@ -11,11 +11,11 @@
 // export const data = () => {
 //  return 'data';
 // };
-function appendChild(element) {
+function createNode(element) {
   return document.createElement(element);
 }
 function append(parent, el) {
-  return parent.appendChild(el);
+  return parent.append(el);
 }
 
 const ul = document.querySelector('#characters');
@@ -24,23 +24,20 @@ const api = 'https://rickandmortyapi.com/api/character/?name=';
 
 function traer() {
   fetch(api + input.value)
-    .then((resp) => resp.json())
-    .then((data) => {
-      const characters = data.results;
-      ul.innerHTML=""
-      return characters.map((character) => {
-        let li = appendChild('li'),
-          img = appendChild('img'),
-         div = appendChild('div');
-        img.src = character.image;
-        characters.innerHTML = `${character.image}`;
-        append(li, img);
+    .then((resp) => resp.json()) // transforma la data en objeto json
+    .then((data) => { // aquí escribimos lo que queremos hacer con esa data
+      const characters = data.results; // esto toma los personajes de la data
+      ul.innerHTML = ''; // esto limpia el html antes de ejecutar la función
+      return characters.map((character) => { // hace un map de los resultados y ejecuta el código
+        let li = createNode('li'), // estos son los elementos necesarios
+          img = createNode('img'),
+         div = createNode('div');
+        img.src = character.image; // aquí hace la búsqueda de la imagen, con la fuente específica de la data
+        characters.innerHTML = `${character.name}`; // hace que se muestre la el nombre en el html
+        append(li, img); // estos son los elementos a adjuntar, falta mostrar el nombre en bloque
         append(li, div);
         append(ul, li);
       });
-    })
-    .catch(function (error) {
-      console.log(error);
     });
 }
 document.querySelector('#cosa').addEventListener('click', traer);
