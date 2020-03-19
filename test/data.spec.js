a3d467768cb1bb2eed4b88d7d45b2b4d1e3db2e9
AndyyAg <andy.ag2303@gmail.com>
2020-03-18 23:36:16 -0600

    Modifiqué test

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,12 +1,10 @@
 // importamos la función `example`
-import { data } from "../src/data";
+import data from '../src/data';
 
 describe('data', () => {
-
   it('debería ser una función', () => {
     expect(typeof data).toBe('function');
   });
-
   describe('data', () => {
     // escribe aquí tu test
   });
