9c10eb2c93d354075598a3edb9f38f89a5b2d05e
Mara Mulato <maramulato@MacBook-Mara.local>
2020-03-21 12:34:41 -0600

    datos en hover

--- src/main.js
+++ src/main.js
@@ -9,32 +9,37 @@ function append(parent, el) {
 const contenido = document.querySelector('#characters');
 const input = document.querySelector('#buscar');
 
-const api = 'https://rickandmortyapi.com/api/character/';
-const searchName = '?name=';
+const api = 'https://rickandmortyapi.com/api/character/?name=';
 
 function createCard(character) {
   // Creamos nodos para mostrar los elementos
   const li = createNode('li');
   const img = createNode('img');
-  const div = createNode('div');
+  const nameChar = createNode('div');
   const charId = createNode('div');
   const location = createNode('div');
+  const gender = createNode('div');
+  const status = createNode('div');
   // Buscamos la data necesaria
   charId.innerHTML = `${character.id}`;
   img.src = character.image;
-  div.innerHTML = `${character.name}`;
+  nameChar.innerHTML = `${character.name}`;
   location.innerHTML = `${character.location.name}`;
+  gender.innerHTML = `${character.gender}`;
+  status.innerHTML = `${character.status}`;
   // La adjuntamos para que quede en el mismo sitio
   append(li, charId);
+  append(li, nameChar);
   append(li, img);
-  append(li, div);
   append(li, location);
+  append(li, gender);
+  append(li, status)
   append(contenido, li);
   return li;
 }
 
 function traer() {
-  fetch(api + searchName + input.value)
+  fetch(api + input.value)
     .then((resp) => resp.json()) // transforma la data en objeto json
     .then((data) => { // aquí escribimos lo que queremos hacer con esa data
       const characters = data.results;
